ad048f13ba7d61161e23f268e5d9c1c694740c01
Elyas Amini <elyas.amini.fs@gmail.com>
2023-09-12 19:48:04 +0330

    Update Contents

--- src/CSharp/EasyMicroservices.QuestionsMicroservice.Domain/Contracts/Requests/UpdateQuestionRequestContract.cs
+++ src/CSharp/EasyMicroservices.QuestionsMicroservice.Domain/Contracts/Requests/UpdateQuestionRequestContract.cs
@@ -1,17 +1,23 @@
 ﻿using EasyMicroservices.QuestionsMicroservice.Contracts.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EasyMicroservices.QuestionsMicroservice.Contracts.Requests
 {
     public class UpdateQuestionRequestContract
     {
+        [JsonIgnore]
+        [BindNever]
+        public string UniqueIdentity { get; set; }
+
         public long Id { get; set; }
         public List<LanguageDataContract> Titles { get; set; }
-        public string UniqueIdentity { get; set; }
 
     }
 }
